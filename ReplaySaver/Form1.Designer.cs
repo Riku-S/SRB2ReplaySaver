320669018d1bae23c67b4b4f33d86bd0d90edfc3
Riku "Ors" Salminen <38985578+Riku-S@users.noreply.github.com>
2021-06-29 20:35:33 +0300

    Add project files.

---
+++ ReplaySaver/Form1.Designer.cs
@@ -0,0 +1,196 @@
+﻿
+namespace ReplaySaver
+{
+    partial class ReplaySaverForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.replayFolderBox = new System.Windows.Forms.TextBox();
+            this.characterBox = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.mapUpDown = new System.Windows.Forms.NumericUpDown();
+            this.label4 = new System.Windows.Forms.Label();
+            this.runButton = new System.Windows.Forms.Button();
+            this.runningLabel = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.mapUpDown)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Replay Saver";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 41.38756F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 58.61244F));
+            this.tableLayoutPanel1.Controls.Add(this.replayFolderBox, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.characterBox, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.label3, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.mapUpDown, 1, 3);
+            this.tableLayoutPanel1.Controls.Add(this.label4, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.label2, 0, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(16, 59);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 4;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(418, 112);
+            this.tableLayoutPanel1.TabIndex = 1;
+            this.tableLayoutPanel1.Paint += new System.Windows.Forms.PaintEventHandler(this.tableLayoutPanel1_Paint);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 5);
+            this.label2.Margin = new System.Windows.Forms.Padding(3, 5, 3, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Replay folder:";
+            // 
+            // replayFolderBox
+            // 
+            this.replayFolderBox.Location = new System.Drawing.Point(176, 3);
+            this.replayFolderBox.Name = "replayFolderBox";
+            this.replayFolderBox.Size = new System.Drawing.Size(239, 20);
+            this.replayFolderBox.TabIndex = 2;
+            // 
+            // characterBox
+            // 
+            this.characterBox.FormattingEnabled = true;
+            this.characterBox.Items.AddRange(new object[] {
+            "All",
+            "Sonic",
+            "Tails",
+            "Knuckles",
+            "Amy",
+            "Fang",
+            "Metal Sonic"});
+            this.characterBox.Location = new System.Drawing.Point(176, 59);
+            this.characterBox.Name = "characterBox";
+            this.characterBox.Size = new System.Drawing.Size(121, 21);
+            this.characterBox.TabIndex = 4;
+            this.characterBox.Text = "All";
+            this.characterBox.SelectedIndexChanged += new System.EventHandler(this.Characters_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(3, 61);
+            this.label3.Margin = new System.Windows.Forms.Padding(3, 5, 3, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Character(s):";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // mapUpDown
+            // 
+            this.mapUpDown.Location = new System.Drawing.Point(176, 87);
+            this.mapUpDown.Name = "mapUpDown";
+            this.mapUpDown.Size = new System.Drawing.Size(120, 20);
+            this.mapUpDown.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(3, 89);
+            this.label4.Margin = new System.Windows.Forms.Padding(3, 5, 3, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(66, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Map number";
+            // 
+            // runButton
+            // 
+            this.runButton.Location = new System.Drawing.Point(14, 199);
+            this.runButton.Name = "runButton";
+            this.runButton.Size = new System.Drawing.Size(75, 23);
+            this.runButton.TabIndex = 3;
+            this.runButton.Text = "Run";
+            this.runButton.UseVisualStyleBackColor = true;
+            this.runButton.Click += new System.EventHandler(this.runButton_Click);
+            // 
+            // runningLabel
+            // 
+            this.runningLabel.AutoSize = true;
+            this.runningLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.runningLabel.Location = new System.Drawing.Point(94, 208);
+            this.runningLabel.Name = "runningLabel";
+            this.runningLabel.Size = new System.Drawing.Size(167, 13);
+            this.runningLabel.TabIndex = 4;
+            this.runningLabel.Text = "<- Press this buttons to start";
+            this.runningLabel.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // ReplaySaverForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(563, 237);
+            this.Controls.Add(this.runningLabel);
+            this.Controls.Add(this.runButton);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "ReplaySaverForm";
+            this.Text = "Replay Saver";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.mapUpDown)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox replayFolderBox;
+        private System.Windows.Forms.ComboBox characterBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown mapUpDown;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button runButton;
+        private System.Windows.Forms.Label runningLabel;
+    }
+}
+
---
+++ ReplaySaver/Form1.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.IO;
+
+namespace ReplaySaver
+{
+    public partial class ReplaySaverForm : Form
+    {
+        bool _running = false;
+        public ReplaySaverForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void Characters_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void SetStatusText(string text)
+        {
+            runningLabel.Invoke((MethodInvoker)delegate { runningLabel.Text = text; });
+        }
+
+        private const int MaxLevelNumber = 1035;
+        public static string MakeMapString(int levelNumber)
+        {
+            var numberForm = "";
+
+            if (levelNumber < 0 || levelNumber > MaxLevelNumber)
+            {
+                return "Invalid-map-number";
+            }
+            if (levelNumber < 100)
+            {
+                // Map number is expressed with two digits
+                numberForm = levelNumber.ToString("D2");
+            }
+            else
+            {
+                // Map number is expressed with a leading letter and a digit/letter
+                var firstChar = (char)((levelNumber - 100) / 36 + 'A');
+                var remainder = (levelNumber - 100) % 36;
+                var secondChar = remainder < 10 ? (char)(remainder + '0') : (char)(remainder - 10 + 'A');
+                char[] chars = { firstChar, secondChar };
+                numberForm = new string(chars);
+            }
+            return "MAP" + numberForm;
+        }
+
+        private void TryToSaveReplay(int mapNum, string character, string replayfolder)
+        {
+            string mapName = MakeMapString(mapNum);
+
+            if (!Directory.Exists(replayfolder))
+            {
+                SetStatusText("Couldn't find your imaginary folder " + replayfolder);
+                return;
+            }
+
+            try
+            {
+                Directory.SetCurrentDirectory(replayfolder);
+                string characterName = character.Trim().ToLower().Replace(" ", "");
+
+                List<string> characters;
+                if (characterName == "all")
+                {
+                    characters = new List<string>(new string[] { "sonic", "tails", "knuckles", "amy", "fang", "metalsonic"});
+                }
+                else
+                {
+                    characters = new List<string>(new string[] { characterName });
+                }
+                foreach (string characterString in characters)
+                {
+
+                    string replayName = mapName + "-" + characterString + "-last.lmp";
+                    if (File.Exists(replayName))
+                    {
+                        for (int i = 0; i < 10000; i++)
+                        {
+                            string newName = "guests/" + mapName + "-" + characterString + "-guest-" + i.ToString("D4") + ".lmp";
+
+                            if (!File.Exists(newName))
+                            {
+                                File.Move(replayName, newName);
+                                SetStatusText("Saved a new guest to " + newName);
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+            catch
+            {
+                SetStatusText("Saving the replay failed. Trying again in a sec...");
+            }
+        }
+
+        private void RunLoop()
+        {
+            int count = 0;
+            while (_running)
+            {
+                count++;
+                int map = 0;
+                string character = "";
+                string folder = "";
+                mapUpDown.Invoke((MethodInvoker)delegate { map = (int)mapUpDown.Value; });
+                characterBox.Invoke((MethodInvoker)delegate { character = characterBox.Text; });
+                replayFolderBox.Invoke((MethodInvoker)delegate { folder = replayFolderBox.Text; });
+
+                TryToSaveReplay(map, character, folder);
+                Thread.Sleep(5000);
+            }
+            Thread.CurrentThread.Abort();
+        }
+        
+        private void runButton_Click(object sender, EventArgs e)
+        {
+            _running = !_running;
+
+            if (_running)
+            {
+                runButton.Text = "Stop";
+                runningLabel.Text = "Running! Go blast some robos!";
+                ThreadStart runStart = new ThreadStart(RunLoop);
+                Thread runThread = new Thread(runStart);
+                runThread.Start();
+            }
+            else
+            {
+                runButton.Text = "Run";
+                runningLabel.Text = "Not running";
+            }
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
